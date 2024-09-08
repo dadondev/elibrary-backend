f797997546c077797528836411eea813d4cf87c5
dadondev <dadondev03@gmail.com>
2024-09-08 11:21:08 -0700

    updated

--- src/validators/compare.validator.ts
+++ src/validators/compare.validator.ts
@@ -3,9 +3,9 @@
 import dateFNS from "date-fns";
 function compareDate(returnDate: Date, todayDate: Date) {
 	const differenceDaysCount = dateFNS.differenceInDays(todayDate, returnDate);
-	if (differenceDaysCount === 1) return "1 day";
-	if (differenceDaysCount < 10) return "earn";
 	if (differenceDaysCount === 0) return "equal";
+	if (differenceDaysCount === 1) return "1 day";
+	if (differenceDaysCount > 0 && differenceDaysCount < 10) return "earn";
 	return "late";
 }
 
