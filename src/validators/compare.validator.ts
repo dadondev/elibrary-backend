21345dc24a30d0db59620179428e3a69a072351d
dadondev <dadondev03@gmail.com>
2024-09-08 11:06:02 -0700

    updated

--- src/validators/compare.validator.ts
+++ src/validators/compare.validator.ts
@@ -2,27 +2,10 @@
 
 import dateFNS from "date-fns";
 function compareDate(returnDate: Date, todayDate: Date) {
-	const firstDate =
-		dateFNS.getDate(returnDate) +
-		":" +
-		dateFNS.getMonth(returnDate) +
-		":" +
-		dateFNS.getYear(returnDate);
-	const secondDate =
-		dateFNS.getDate(todayDate) +
-		":" +
-		dateFNS.getMonth(todayDate) +
-		":" +
-		dateFNS.getYear(todayDate);
-
-	if (
-		dateFNS.getDate(todayDate) - dateFNS.getDate(returnDate) === 1 &&
-		dateFNS.getMonth(todayDate) === dateFNS.getMonth(returnDate) &&
-		dateFNS.getYear(todayDate) === dateFNS.getYear(returnDate)
-	)
-		return "1 day";
-	if (firstDate < secondDate) return "earn";
-	if (firstDate === secondDate) return "equal";
+	const differenceDaysCount = dateFNS.differenceInDays(todayDate, returnDate);
+	if (differenceDaysCount === 1) return "1 day";
+	if (differenceDaysCount < 10) return "earn";
+	if (differenceDaysCount === 0) return "equal";
 	return "late";
 }
 
