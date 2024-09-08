418581db81f67a0b23a8510090bed8072d3f8d33
dadondev <dadondev03@gmail.com>
2024-09-08 11:07:43 -0700

    updated

--- src/models/catalog.schema.ts
+++ src/models/catalog.schema.ts
@@ -1,54 +1,57 @@
-import * as mongoose from 'mongoose';
+/** @format */
 
+import * as mongoose from "mongoose";
 
 const parentSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    phoneNumber:{
-        type:String,
-        required:true
-    },
-    role:{
-        type:String,
-        required:true
-    },
-    job:{
-        type:String,
-        required:true
-    }
-})
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	job: {
+		type: String,
+		required: true,
+	},
+});
 
 const catalogSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    phoneNumber:{
-        type:String,
-    },
-    address:{
-        type:String,
-        required:true,
-        minLength:10,
-        maxLength:200
-    },
-    parents:{
-        type:[parentSchema]
-    },
-    birthday:{
-        type:Date,
-        reuqired:true
-    }
-})
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	phoneNumber: {
+		type: String,
+		unique: true,
+	},
+	address: {
+		type: String,
+		required: true,
+		minLength: 10,
+		maxLength: 200,
+	},
+	parents: {
+		type: [parentSchema],
+	},
+	birthday: {
+		type: Date,
+		reuqired: true,
+	},
+});
 
-export default mongoose.model("catalog", catalogSchema)
+export default mongoose.model("catalog", catalogSchema);
