86f3d97c075b7103eec6e67cb1565bcc8ab2d6c5
dadondev <dadondev03@gmail.com>
2024-09-08 11:30:57 -0700

    fix compare date

--- src/validators/compare.validator.ts
+++ src/validators/compare.validator.ts
@@ -2,11 +2,12 @@
 
 import dateFNS from "date-fns";
 function compareDate(returnDate: Date, todayDate: Date) {
-	const differenceDaysCount = dateFNS.differenceInDays(todayDate, returnDate);
+	const differenceDaysCount = dateFNS.differenceInDays(returnDate, todayDate);
 	if (differenceDaysCount === 0) return "equal";
 	if (differenceDaysCount === 1) return "1 day";
 	if (differenceDaysCount > 0 && differenceDaysCount < 10) return "earn";
 	return "late";
 }
+
 
 export default compareDate;
