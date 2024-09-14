ebe07fa95fa3bdc1a972faf1e4ac0e27bbd5c4e3
dadondev <dadondev03@gmail.com>
2024-09-14 22:26:48 +0500

    salom

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import authRouter from "./routes/auth.routes";
 import { authMiddleware } from "./middlewares/auth.middleware";
 
 const corsOptions = {
-	origin: ["http://localhost:3000", "https://quiz-app-clone.vercel.app/"],
+	origin: ["http://localhost:3000", "https://quiz-app-clone.vercel.app/", "*"],
 	methods: ["GET", "POST", "DELETE", "PATCH", "PUT"],
 	credentials: true,
 	optionsSuccessStatus: 200,
